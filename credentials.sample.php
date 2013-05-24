1fd6d6786171cc9c68de54751bd337f31372e3d5
Michele Locati <michele@locati.it>
2013-05-24 13:18:51 +0200

    Added a sample credentials.php file

---
+++ credentials.sample.php
@@ -0,0 +1,11 @@
+<?php
+/* This is a sample credetials.php file */
+
+/** The Transifex user name.
+* @var string
+*/
+define('TRANSIFEX_USERNAME', 'The Transifex username');
+/** The Transifex password.
+* @var string
+*/
+define('TRANSIFEX_PASSWORD', 'The Transifex password');
