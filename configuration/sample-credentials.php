b0d4afb46ad0c2ad7fe6d6a63a3f52e9bb39f2a8
Michele Locati <michele@locati.it>
2014-03-20 17:38:48 +0100

    Add DB configuration to sample-credentials

--- configuration/sample-credentials.php
+++ configuration/sample-credentials.php
@@ -9,3 +9,19 @@ define('C5TT_TRANSIFEX_USERNAME', 'The Transifex username');
 * @var string
 */
 define('C5TT_TRANSIFEX_PASSWORD', 'The Transifex password');
+/** The database host.
+* @var string
+*/
+define('C5TT_DB_SERVER', 'db_server');
+/** The database username.
+* @var string
+*/
+define('C5TT_DB_DATABASE', 'db_username');
+/** The database password.
+* @var string
+*/
+define('C5TT_DB_USERNAME', 'db_user');
+/** The database name.
+* @var string
+*/
+define('C5TT_DB_PASSWORD', 'db_password');
